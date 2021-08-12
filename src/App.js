ceedd0548e7a79d55f4f3711a127733c9afe644d
Zandro Gene Ortecio <kyahandoy@gmail.com>
2021-08-13 05:24:42 +0800

    fourth commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div><h1>See you next year! - Rudy Balbin</h1></div>
+    <div className="App">
+      <header className="App-header">
+      <h1>See you next year! - Rudy Balbin</h1>
+      </header>
+    </div>
+   
   );
 }
 
