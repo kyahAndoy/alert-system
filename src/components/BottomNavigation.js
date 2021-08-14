b39cd651e2b79c35fdaedcfdec17b263d6ab3519
Zandro Gene Ortecio <kyahandoy@gmail.com>
2021-08-14 20:02:26 +0800

    bottomNav

---
+++ src/components/BottomNavigation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import useStyle from './compStyles/BottomNavStyle';
+
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const BottomNavigation = () => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    return (
+        <>
+            <BottomNavigation>
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+            </BottomNavigation>
+        </>
+    )
+}
+
+export default BottomNavigation;
---
+++ src/components/compStyles/BottomNavStyle.js
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles({
+
+})
+
+export default useStyle;
