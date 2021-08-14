a57919efef7aea7564f7812eda08afe4e36602a7
Zandro Gene Ortecio <kyahandoy@gmail.com>
2021-08-14 20:13:25 +0800

    UserPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
+import UserPage from './UserPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>See you next year! - Rudy Balbin</h1>
-      </header>
-    </div>
-
+    <Router>
+            <Switch>
+              <Route exact path="/" component={UserPage} />
+              {/* <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/welcome" component={TransactionType} />
+              <Route path="/bir-transactions" component={BIRTransactions} />
+              <Route path="/profiles" component={Profiles} />
+              <Route path="/profile-creation" component={ProfileCreationForm} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/tax-filing" component={TaxFilingForm} />
+              <Route path="/payment" component={Payment} />
+              <Route path="/*" component={Page404} /> */}
+            </Switch>
+          </Router>
   );
 }
 
---
+++ src/UserPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import BottomNavigation from './components/BottomNavigation';
+
+
+class UserPage extends Component {
+    render() {
+        return (
+            <BottomNavigation></BottomNavigation>
+        )
+    }
+}
+
+export default UserPage;
