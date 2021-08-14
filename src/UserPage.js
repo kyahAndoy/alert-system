d0aa190be31725046ed50a8e5a030f69b8c90209
Zandro Gene Ortecio <kyahandoy@gmail.com>
2021-08-14 21:47:17 +0800

    components

--- src/UserPage.js
+++ src/UserPage.js
@@ -6,7 +6,7 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
-import useStyles from './compStyles/BottomNavStyle';
+import useStyles from './components/compStyles/BottomNavStyle';
 
 const UserPage = () => {
      const classes = useStyles();
