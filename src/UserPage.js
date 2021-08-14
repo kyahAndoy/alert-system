2045a285c4e0f1f23b5a3b85db4f5e7bcf6029b6
Zandro Gene Ortecio <kyahandoy@gmail.com>
2021-08-14 21:22:02 +0800

    move the bottom nav

--- src/UserPage.js
+++ src/UserPage.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import Container from '@material-ui/core/Container';
-import BottomNav from './components/BottomNav';
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 
 class UserPage extends Component {
     render() {
+       
+        const [value, setValue] = React.useState(0);
         return (
-            <Container maxWidth="xs">
-                <BottomNav></BottomNav>
+            <Container>
+                 <BottomNavigation
+                
+                value={value}
+                onChange={(event, newValue) => {
+                     setValue(newValue);
+                }}
+      showLabels
+            >
+                <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+                <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+                <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+            
+            </BottomNavigation>
             </Container>
         )
     }
